e91e1a96c854768ed7860ac3fa923b8381a7fd83
Anne Speck <anne.speck@returnpath.com>
2016-12-08 16:16:40 -0700

    code for correct answer to 3a
    Change-Id: Ie15df72f5d0fd8725b58e9cceee40deac2b3457e

--- day3a.pl
+++ day3a.pl
@@ -1,34 +1,43 @@
 #!/usr/bin/perl
 
-$a = 0; $b = 0; $c = 0; 
-$count = 0; $validcount = 0; 
-$filename = 'day3input.txt';  
+$a = 0; $b = 0; $c = 0;
+$count = 0; $validcount = 0;
+$filename = 'day3input.txt';
 
 open($datain,"<",$filename) or die "Could not open $filename $!";
 
 while ($dataline = <$datain>){
-	
-	chomp $dataline; 
-	($a, $b, $c) = split / /, $dataline; 
-	
-	print "$count: a: $a b: $b c: $c \n"; 
-	
-	if ((($a + $b) >= $c) && (($a + $c) >= $b) && (($b + $c) >=$a))
-	{
-		print "valid \n";
-		$validcount++; 
-	} else {
-		print "invalid \n";
-	}
-	$count++;
+
+    chomp $dataline;
+    ($a, $b, $c) = split / /, $dataline;
+
+   	print "$count: a: $a b: $b c: $c \t";
+
+    if (($a + $b) > $c){
+      if (($a + $c) > $b){
+       if (($b + $c) >$a) {
+        print "valid \n";
+        $validcount++;
+       } else {
+        print "invalid \n";
+       }
+      }else {
+        print "invalid \n";
+      }
+
+    }else {
+        print "invalid \n";
+    }
+
+    $count++;
 }
 
-print "Number of valid triangles: $validcount total rows: $count\n"; 
+print "Number of valid triangles: $validcount total rows: $count\n";
 
-close $datain; 
-	
-	
-	
-	
+close $datain;
 
- 
+
+
+
+
+
